14eb8380040041391e2201f6e1268ec73280fccd
Jacobo Moral Buendía <jacobo.moral@gmail.com>
2018-10-30 11:39:53 +0100

    changes to AES source code

---
+++ Practica 2/Exercici 3/ex1/Desxifra_1.py
@@ -0,0 +1,14 @@
+from Crypto.Cipher import AES
+
+key = open("albert.key", 'rb').read()
+enc = open("albert.enc", 'rb').read()
+iv = enc[:AES.block_size]
+#figue
+cipher = AES.new(key, AES.MODE_CFB, iv)
+
+#jacobo
+cipher = AES.new(key, AES.MODE_CBC, iv)
+
+plaintext = cipher.decrypt(enc[AES.block_size:])
+open('soluciones/albert.jpeg', 'wb').write(plaintext)
+open('soluciones/albert.html', 'wb').write(plaintext)
