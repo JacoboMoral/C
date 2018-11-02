a7d32c92fe4384a277aec01b4bb480fab22b2562
Jacobo Moral Buendía <jacobo.moral@gmail.com>
2018-11-02 07:19:58 +0100

    byte_sub start

--- Practica 2/Exercici 2/ex1/AES_sub_bytes.py
+++ Practica 2/Exercici 2/ex1/AES_sub_bytes.py
@@ -1,5 +1,5 @@
 # source:  https://github.com/boppreh/aes/blob/master/aes.py
-
+from random import randint
 s_box = (
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
     0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -19,50 +19,15 @@ s_box = (
     0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16,
 )
 
-inv_s_box = (
-    0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
-    0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
-    0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
-    0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
-    0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
-    0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
-    0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
-    0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
-    0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
-    0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
-    0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
-    0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
-    0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
-    0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
-    0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
-    0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
-)
-
-
 def sub_bytes(s):
-    for i in range(4):
-        for j in range(4):
-            s[i][j] = s_box[s[i][j]]
+    return
 
 
-
-def inv_sub_bytes(s):
-    for i in range(4):
-        for j in range(4):
-            s[i][j] = inv_s_box[s[i][j]]
-
-
-#el codi original era realment un "shift_columns", com esta a la funcio "inv_shift_rows", que no hem tocat
 def shift_rows(s):
-    s[1][0], s[1][1], s[1][2], s[1][3] = s[1][1], s[1][2], s[1][3], s[1][0]
-    s[2][0], s[2][1], s[2][2], s[2][3] = s[2][2], s[2][3], s[2][0], s[2][1]
-    s[3][0], s[3][1], s[3][2], s[3][3] = s[3][3], s[3][0], s[3][1], s[3][2]
-
-
-def inv_shift_rows(s):
-    s[0][1], s[1][1], s[2][1], s[3][1] = s[3][1], s[0][1], s[1][1], s[2][1]
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[1][1], s[2][1], s[3][1], s[0][1]
     s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
-    s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
+
 
 def add_round_key(s, k):
     for i in range(4):
@@ -88,18 +53,6 @@ def mix_columns(s):
     for i in range(4):
         mix_single_column(s[i])
 
-
-def inv_mix_columns(s):
-    # see Sec 4.1.3 in The Design of Rijndael
-    for i in range(4):
-        u = xtime(xtime(s[i][0] ^ s[i][2]))
-        v = xtime(xtime(s[i][1] ^ s[i][3]))
-        s[i][0] ^= u
-        s[i][1] ^= v
-        s[i][2] ^= u
-        s[i][3] ^= v
-
-    mix_columns(s)
 
 
 r_con = (
@@ -132,16 +85,6 @@ def pad(plaintext):
     padding = bytes([padding_len] * padding_len)
     return plaintext + padding
 
-def unpad(plaintext):
-    """
-    Removes a PKCS#7 padding, returning the unpadded text and ensuring the
-    padding was correct.
-    """
-    padding_len = plaintext[-1]
-    assert padding_len > 0
-    message, padding = plaintext[:-padding_len], plaintext[-padding_len:]
-    assert all(p == padding_len for p in padding)
-    return message
 
 
 class AES(object):
@@ -202,13 +145,11 @@ class AES(object):
         assert len(plaintext) == 16
 
         plain_state = bytes2matrix(plaintext)
-
         add_round_key(plain_state, self._key_matrices[0])
 
-        #el codi original només feia una iteració en aquest loop. Per tant, no feia les 9+1 iteracions
-        #necessaries segons AES-128, sino que feia 1+1.
+        #el codi original nomes feia 1 iteracio en aquest bucle i una altra fora, en comptes de
+        #9 iteracions + 1 fora en el cas de AES-128
         for i in range(0, self.n_rounds-1, 1):
-            print(i)
             sub_bytes(plain_state)
             shift_rows(plain_state)
             mix_columns(plain_state)
@@ -220,28 +161,6 @@ class AES(object):
 
         return matrix2bytes(plain_state)
 
-    def decrypt_block(self, ciphertext):
-        """
-        Decrypts a single block of 16 byte long ciphertext.
-        """
-        assert len(ciphertext) == 16
-
-        cipher_state = bytes2matrix(ciphertext)
-
-        add_round_key(cipher_state, self._key_matrices[-1])
-        inv_shift_rows(cipher_state)
-        inv_sub_bytes(cipher_state)
-
-        for i in range(self.n_rounds - 1, 0, -1):
-            add_round_key(cipher_state, self._key_matrices[i])
-            inv_mix_columns(cipher_state)
-            inv_shift_rows(cipher_state)
-            inv_sub_bytes(cipher_state)
-
-        add_round_key(cipher_state, self._key_matrices[0])
-
-        return matrix2bytes(cipher_state)
-
     def encrypt_cbc(self, plaintext, iv):
         """
         Encrypts `plaintext` using CBC mode and PKCS#7 padding, with the given
@@ -250,38 +169,14 @@ class AES(object):
         assert len(iv) == 16
 
         plaintext = pad(plaintext)
-
         blocks = []
         previous = iv
-        # Splits in 16-byte parts.
-        for i in range(0, len(plaintext), 16):
-            plaintext_block = plaintext[i:i+16]
-            escriuBloc(-1,plaintext_block)
-            # CBC mode encrypt: encrypt(plaintext_block XOR previous)
-            block = self.encrypt_block(xor_bytes(plaintext_block, previous))
-            blocks.append(block)
-            previous = block
-            escriuBloc(i,block)
-
+        plaintext_block = plaintext[0:16]
+        # CBC mode encrypt: encrypt(plaintext_block XOR previous)
+        block = self.encrypt_block(xor_bytes(plaintext_block, previous))
+        blocks.append(block)
+        previous = block
         return b''.join(blocks)
-
-    def decrypt_cbc(self, ciphertext, iv):
-        """
-        Decrypts `plaintext` using CBC mode and PKCS#7 padding, with the given
-        initialization vector (iv).
-        """
-        assert len(iv) == 16
-
-        blocks = []
-        previous = iv
-        # Splits in 16-byte parts.
-        for i in range(0, len(ciphertext), 16):
-            ciphertext_block = ciphertext[i:i+16]
-            # CBC mode decrypt: previous XOR decrypt(ciphertext)
-            blocks.append(xor_bytes(previous, self.decrypt_block(ciphertext_block)))
-            previous = ciphertext_block
-        return unpad(b''.join(blocks))
-
 
 import os
 from hashlib import pbkdf2_hmac
@@ -316,75 +211,123 @@ def encrypt(key, plaintext, workload=100000):
         key = key.encode('utf-8')
     if isinstance(plaintext, str):
         plaintext = plaintext.encode('utf-8')
-
     salt = os.urandom(SALT_SIZE)
     key, hmac_key, iv = get_key_iv(key, salt, workload)
     ciphertext = AES(key).encrypt_cbc(plaintext, iv)
     hmac = new_hmac(hmac_key, salt + ciphertext, 'sha256').digest()
     assert len(hmac) == HMAC_SIZE
 
-    return hmac + salt + ciphertext
+    #return hmac + salt + ciphertext
+    return ciphertext
 
-
-def decrypt(key, ciphertext, workload=100000):
-    """
-    Decrypts `plaintext` with `key` using AES-128, an HMAC to verify integrity,
-    and PBKDF2 to stretch the given key.
-    The exact algorithm is specified in the module docstring.
-    """
-
-    assert len(ciphertext) % 16 == 0, "Ciphertext must be made of full 16-byte blocks."
-
-    assert len(ciphertext) >= 32, """
-    Ciphertext must be at least 32 bytes long (16 byte salt + 16 byte block). To
-    encrypt or decrypt single blocks use `AES(key).decrypt_block(ciphertext)`.
-    """
-
-    if isinstance(key, str):
-        key = key.encode('utf-8')
-
-    hmac, ciphertext = ciphertext[:HMAC_SIZE], ciphertext[HMAC_SIZE:]
-    salt, ciphertext = ciphertext[:SALT_SIZE], ciphertext[SALT_SIZE:]
-    key, hmac_key, iv = get_key_iv(key, salt, workload)
-
-    expected_hmac = new_hmac(hmac_key, salt + ciphertext, 'sha256').digest()
-    assert compare_digest(hmac, expected_hmac), 'Ciphertext corrupted or tampered.'
-
-    return AES(key).decrypt_cbc(ciphertext, iv)
-
-
-def escriuBlocs(blocs):
-    #blocs es un array de blocs de 16 bytes (128 bits) en format binari
-    for i in range(0, len(blocs)):
-        print(len(blocs))
 
 def escriuBloc(i, bloc):
-    #bloc es un bloc de 16 bytes (128 bits)
     if i >= 0:
         print("Bloc numero: " + str(i//16))
+
+    #bloc es un bloc de 16 bytes (128 bits)
     hexNumber = bloc.hex()
-    i = 1
-    for h in hexNumber:
-        print(h, end='')
-        if (i%2 == 0 and i%8 != 0):
-            print(" ", end='')
-        if(i%8 == 0):
-            print()
-        i = i + 1;
-    print()
+    matriu = [[0 for x in range(4)] for y in range(4)]
 
-def main():
-    listBloc= [[7, 160, 51, 115], [55, 167, 50, 43], [125, 16, 28, 225], [141, 20, 69, 161]]
-    #07 a0 33 73
-    #37 a7 32 2b
-    #7d 10 1c e1
-    #8d 14 45 a1
-    bytesBloc = matrix2bytes(listBloc)
+    for i in range(4):
+        for j in range(4):
+            matriu[j][i] = hexNumber[i*8+j*2] + hexNumber[i*8+j*2+1];
 
+    for i in range(4):
+        for j in range(4):
+            print(matriu[i][j], end=' ')
+        print()
+
+def escriuBlocFile(file, bloc, missatge):
+    #bloc es un bloc de 16 bytes (128 bits)
+    hexNumber = bloc.hex()
+    matriu = [[0 for x in range(4)] for y in range(4)]
+
+    for i in range(4):
+        for j in range(4):
+            matriu[j][i] = hexNumber[i*8+j*2] + hexNumber[i*8+j*2+1];
+    file.write(missatge + "\n")
+    for i in range(4):
+        for j in range(4):
+            file.write(str(matriu[i][j]) + " ")
+        file.write("\n")
+    file.write("\n")
+
+def emplenaRandom(bloc):
+    #nomes emplena blocs de 4x4
+    for i in range(4):
+        bloc[i][0] = randint(0,255)
+        bloc[i][1] = randint(0,255)
+        bloc[i][2] = randint(0,255)
+        bloc[i][3] = randint(0,255)
+
+def canviaLetraString(string, index, lletra):
+    string = list(string)
+    string[index] = lletra
+    return("".join(string))
+
+
+def canviabit(num, index):
+    numBinari = format(num,'08b')
+    b = numBinari[index]
+    if (b == "0"):
+        b = "1"
+    else:
+        b = "0"
+    numeroEnBinari = canviaLetraString(numBinari, index, b)
+    return int(numeroEnBinari,base=2)
+
+def canviabits(num, index1, index2):
+    numBinari = format(num,'08b')
+    b1 = numBinari[index1]
+    if (b1 == "0"):
+        b1 = "1"
+    else:
+        b1 = "0"
+
+    b2 = numBinari[index2]
+    if (b2 == "0"):
+        b2 = "1"
+    else:
+        b2 = "0"
+    numeroEnBinari = canviaLetraString(numBinari, index, b1)
+    numeroEnBinari = canviaLetraString(numeroEnBinari, index, b2)
+    return int(numeroEnBinari,base=2)
+
+
+# Canviem la funcio ByteSub per la identitat, i.e. ByteSub(x)=x
+def exercici():
     key = 91988770966827344886319470096581337551
     key = key.to_bytes(16, byteorder='big', signed=True)
+    missatgeBloc = [[None]*4 for _ in range(4)]
+    file = open("shift_rows.txt","w")
+    print("Escrivint ...")
 
-    encryptedFile = encrypt(key, bytesBloc)
+    #generem 10 missatges aleatoris de mida d'un bloc
+    for l in range(10):
+        emplenaRandom(missatgeBloc)
+        encryptedFile = encrypt(key, matrix2bytes(missatgeBloc))
+        escriuBlocFile(file,encryptedFile,"bloc original numero # " + str(l+1) + "encriptat: \n")
+        for i in range(4):
+            for j in range(4):
+                originalValue = missatgeBloc[i][j]
+                for k in range(8):
+                    for m in range(8):
+                        numi = canviabit(missatgeBloc[i][j],k)
+                        numj = canviabit(missatgeBloc[i][j],k)
+                        missatgeBloc[i][j] = num
+                        escriuBloc(matrix2bytes(missatgeBloc))
+                        missatgeBloc[i][j] = originalValue
 
+
+                        encryptedFile = encrypt(key, matrix2bytes(missatgeBloc))
+                        missatge = "#"+str(l+1) + " bloc encriptat despres de canviar el bit numero " + str(127-i*32-j*8-k)
+                        missatgeBloc[i][j] = originalValue
+
+
+#exercici 2.1.1
+def main():
+
+    exercici()
 
 main()
