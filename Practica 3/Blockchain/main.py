d7fb55c61c4e88b184c77ad64169b416bb6fb503
Jacobo Moral <jacobo.moral@gmail.com>
2018-11-28 12:12:51 +0100

    fix some .txt error

--- Practica 3/Blockchain/main.py
+++ Practica 3/Blockchain/main.py
@@ -3,6 +3,7 @@ from rsa_key import rsa_key
 from transaction import transaction
 from block_chain import block_chain
 
+##JACOBO
 
 myMessage = str(random.randrange(0,2**256))
 myPrivateKey = rsa_key()
@@ -17,10 +18,101 @@ for i in range(99):
      myTransaction = transaction(myMessage, myPrivateKey)
      myBlockchain.add_block(myTransaction)
 
-
-print("Verificando blockchain...")
+print("Verificando blockchain de 100 bloques de Jacobo Moral...")
 if myBlockchain.verify():
-    print("Verificada correctament")
+    print("Verificada correctament la primera blockchain de Jacobo")
 else:
     print("Blockchain no valida")
-myBlockchain.writeFile("blockchain.txt")
+myBlockchain.writeFile("blockchain_100_jacobo")
+
+
+#__________________________________________________________
+
+
+myMessage = str(random.randrange(0,2**256))
+myPrivateKey = rsa_key()
+myTransaction = transaction(myMessage, myPrivateKey)
+xx = 92 #Jacobo Moral. Dni: xxxx7092
+
+#es crea una nova blockchain amb un bloc, el genesis
+myBlockchain = block_chain(myTransaction)
+
+for i in range(xx):
+     myMessage = str(random.randrange(0,2**256))
+     myPrivateKey = rsa_key()
+     myTransaction = transaction(myMessage, myPrivateKey)
+     myBlockchain.add_block(myTransaction)
+
+
+for i in range(99-xx):
+    myMessage = str(random.randrange(0,2**256))
+    myPrivateKey = rsa_key()
+    myTransaction = transaction(myMessage, myPrivateKey)
+    myBlockchain.add_block_invalid(myTransaction)
+
+
+print("Verificando blockchain de 100 - xx bloques de Jacobo Moral...")
+if myBlockchain.verify():
+    print("Verificada correctament la segunda blockchain de Jacobo")
+else:
+    print("Blockchain no valida")
+myBlockchain.writeFile("blockchain_100_xx_jacobo")
+
+
+#__________________________________________________________
+#__________________________________________________________
+
+
+##ALBERT
+
+myMessage = str(random.randrange(0,2**256))
+myPrivateKey = rsa_key()
+myTransaction = transaction(myMessage, myPrivateKey)
+
+#es crea una nova blockchain amb un bloc, el genesis
+myBlockchain = block_chain(myTransaction)
+
+for i in range(99):
+     myMessage = str(random.randrange(0,2**256))
+     myPrivateKey = rsa_key()
+     myTransaction = transaction(myMessage, myPrivateKey)
+     myBlockchain.add_block(myTransaction)
+
+print("Verificando blockchain de 100 bloques de Albert Figuera...")
+if myBlockchain.verify():
+    print("Verificada correctament la primera blockchain de Albert")
+else:
+    print("Blockchain no valida")
+myBlockchain.writeFile("blockchain_100_albert")
+
+
+#__________________________________________________________
+
+myMessage = str(random.randrange(0,2**256))
+myPrivateKey = rsa_key()
+myTransaction = transaction(myMessage, myPrivateKey)
+xx = 72 #Albert Figuera. Dni: xxxx8272
+
+#es crea una nova blockchain amb un bloc, el genesis
+myBlockchain = block_chain(myTransaction)
+
+for i in range(xx):
+     myMessage = str(random.randrange(0,2**256))
+     myPrivateKey = rsa_key()
+     myTransaction = transaction(myMessage, myPrivateKey)
+     myBlockchain.add_block(myTransaction)
+
+
+for i in range(99-xx):
+     myMessage = str(random.randrange(0,2**256))
+     myPrivateKey = rsa_key()
+     myTransaction = transaction(myMessage, myPrivateKey)
+     myBlockchain.add_block_invalid(myTransaction)
+
+
+print("Verificando blockchain de 100 - xx bloques de Albert Figuera...")
+if myBlockchain.verify():
+    print("Verificada correctament la segunda blockchain de Albert")
+else:
+    print("Blockchain no valida")
+myBlockchain.writeFile("blockchain_100_xx_albert")
